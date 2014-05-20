5360f10b4b80403a0e676b430c07b2532c6ccc47
Bruna Peres <buhk06@gmail.com>
2014-05-20 11:12:53 -0300

    Update confere_gabarito.py

--- tp2/confere_gabarito.py
+++ tp2/confere_gabarito.py
@@ -26,7 +26,7 @@ def main():
     #Para permitir o uso de pop nessa lista na ordem de leitura do arquivo
     linhas_gabarito.reverse()
 
-    print "Testando instância %s (%d consultas)" % (nome_instancia, num_linhas_gabarito);
+    print "Testando algoritmo exato (%d consultas)" % (num_linhas_gabarito);
 
     pesos_instancias.append(num_linhas_gabarito);
 
@@ -77,24 +77,16 @@ def main():
 
 
     #Confere se a heuristica executa corretamente em um tempo aceitavel
-    nome_gabarito = dir_gabarito + "heuristica.sol";
     nome_entrada = dir_gabarito + "heuristica.in";
     nome_saida_teste = "heuristica.out";
 
     nota = 10;
 
-
-    linhas_gabarito = [];
-    with open(nome_gabarito, 'r') as arq_gabarito:
-        for linha in arq_gabarito:
-            if linha != '\n':
-                linhas_gabarito.append(linha.strip());
-
-    num_linhas_gabarito = len(linhas_gabarito);
+    num_linhas_gabarito = 100;
     #Para permitir o uso de pop nessa lista na ordem de leitura do arquivo
-    linhas_gabarito.reverse()
+   
 
-    print "Testando instância %s (%d consultas)" % (nome_instancia, num_linhas_gabarito);
+    print "Testando heuristica (100 consultas)";
 
     pesos_instancias.append(num_linhas_gabarito);
 
@@ -110,28 +102,20 @@ def main():
     memoria_max = int(output[-15].split(" ")[-1])/1024.
     tempo_exec = float(output[-22].split(" ")[-1])+float(output[-23].split(" ")[-1])
 
-
+    linhas_saida = [];
     num_acertos = 0;
     num_linhas_teste = 0;
     with open(nome_saida_teste, 'r') as arq_teste:
         for linha in arq_teste:
            if linha == "\n":
-               continue;
+               linhas_saida.append(linha.strip());
+    num_linhas_saida = len(linhas_saida);
                 
-            if len(linhas_gabarito) <= 0:
+    if num_linhas_saida != num_linhas_gabarito:
             	print "Número de consultas no arquivo de teste maior que o número de consultas no gabarito!";
                	break;
 		nota = nota/2; #perde metade dos pontos da heuristica
 
-            if linha.strip() == linhas_gabarito.pop():
-                num_acertos += 1;
-
-            num_linhas_teste += 1;
-
-    if num_linhas_teste != num_linhas_gabarito:
-        print "Número de consultas do arquivo de teste diferente do gabarito! (%d/%d)" % (num_linhas_teste, num_linhas_gabarito);
-
-       
     if
     print "Nota na execução da heurística (0 a 10): %d" % nota;
     print "Gasto máximo de memória: %.2f MB" % memoria_max;
