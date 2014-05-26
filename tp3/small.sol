f515c3865b5817242d693991ff2d6e452f4c5c07
Bruna Peres <buhk06@gmail.com>
2014-05-26 09:40:24 -0300

    Create small.sol

---
+++ tp3/small.sol
@@ -0,0 +1,320 @@
+a 7 4 8 1 3 2
+accent 2
+ago 3
+alike 3
+all 4
+almost 9 1
+and 3 7 2 1 5
+another 6
+are 8 9
+arrives 1
+as 7 8 4 9 2
+at 9 2 5 3 1
+bad 3
+bag 3
+basil 4
+beaten 7
+beautiful 9
+bed 1 2
+bedroom 5
+begged 6
+belly 5
+black 8
+blackberries 1
+body 6
+boots 10
+born 7
+bothering 4
+bowl 3
+braid 1
+bright 2
+brightens 3
+broken 7
+brought 4
+bucket 4
+but 2 7 6
+buttercup 1 9
+can 9
+canvas 2
+cap 1
+capitol 2
+carefully 4
+cat 10
+catches 8
+cats 3
+cheeks 6
+cheese 4
+cinder 8
+clean 8
+climbed 3
+closed 9
+closer 4
+closest 2
+coal 4 7
+coat 2
+cocooned 6
+cold 1
+color 1
+come 2
+course 3
+cover 2
+crawling 5 4
+cried 6
+curled 5
+dark 1
+day 3 4
+did 3
+district 4
+distrusts 3
+dont 4
+down 7
+dreams 3
+drown 4
+dust 7
+ear 10
+effie 2
+elbow 4
+empty 8
+enough 5
+entrails 9
+even 3 6 8 7 4
+ever 2
+expression 3
+eyes 1 4
+face 7
+faces 7
+falls 2
+feast 3
+feed 6 9
+feel 4
+feeling 7
+feet 10
+few 1
+finding 2
+fingers 1
+fleas 5
+flower 2
+for 8
+forage 3
+forgot 1
+fresh 7
+from 3 1
+gale 7
+games 1
+gift 4
+goat 4
+got 7
+grab 1
+gray 8
+guarding 10
+had 3 6
+half 10
+happy 1
+hard 6
+has 9 10
+hates 2
+have 2 6 3
+he 2 3 9 1
+heading 5
+her 5 10
+hes 7
+him 1 4 6
+his 1 3
+hissing 9
+home 4
+hour 5
+house 9
+houses 8
+how 4
+hunched 5
+hunger 1
+hungry 3
+hunting 10
+i 1 4 2 3 5 6 8 9
+if 9
+in 3 5 6 10 4 9
+insisting 1
+into 10 1 2
+is 1 3 7 10 9 4
+isnt 9
+it 3 7
+kill 8
+kitten 5
+knees 9
+knuckles 6
+last 5
+leaping 1
+least 3
+leather 10
+leaves 4
+left 4
+legs 2
+let 6
+light 5
+lines 7
+little 5 4
+long 1 6
+looks 6
+love 2
+lovely 7
+luck 4
+man 7
+maniacally 2
+many 6
+mashed 10
+matched 2
+mattress 2
+may 9
+me 9 2 3 4
+men 5
+mimics 2
+miners 4
+missing 10
+molded 10
+morning 5 7
+mother 3 6 9 7
+mothers 6
+mouser 8
+mouth 6
+muddy 2
+must 2
+my 1 5 6 9 7 2 10 3 4
+myself 4
+nails 7
+named 8 1
+names 1
+needed 5
+nicknamed 4
+no 9
+nose 10
+not 7 4
+occasional 8
+of 1 2 3 10 7 4
+off 2
+okay 7
+old 7
+on 4 5 1 3 8
+once 9 1
+one 4 10
+only 2
+or 9 2
+other 1
+our 3 4 9
+out 1 7 5 3
+outside 4
+part 4
+perfect 4
+plucks 1
+pocket 4
+pressed 6
+prim 5 1 4 6 3
+primrose 8
+prims 2 7 9 4
+prop 4
+protect 3
+pull 1 4
+put 4
+raindrop 7
+rat 8
+rats 3
+read 1
+real 3
+reaping 4 9
+remembers 3
+rid 7
+roll 4
+rotting 1
+rough 2
+say 4
+says 7
+scrawny 4
+scrub 6
+seam 4
+see 5
+seeking 1
+sentimental 7
+she 2 3 8
+shift 5
+shirt 1
+shoulders 5
+shutters 8
+side 1 5
+since 6
+sister 5
+sits 4
+sitting 9
+sleep 6 9
+slide 10
+slip 4
+so 7 9 6
+sometimes 8
+squash 1
+squat 8
+squirrel 7
+stay 7
+still 7 3
+stopped 9 6
+streets 8
+stretch 1
+suddenly 3
+sunken 7
+supple 10
+swing 2
+swollen 5 6
+table 3
+tell 9
+thank 3
+that 1 10
+the 1 2 3 5 8 10 7 9 4 6
+their 6 7
+them 5
+theres 4
+they 9
+thing 5
+think 3 7
+this 3 9 5 7
+though 3
+to 5 4 6 2 10 3 1
+today 7 4
+together 6
+too 9
+treat 3
+tried 4
+trinket 2
+trousers 1
+trying 6
+tuck 1
+turned 7
+two 9
+ugliest 10
+under 3
+until 9
+up 1 4 5
+upbeat 2
+us 4
+usually 4
+vermin 7
+very 9
+wake 1
+warmth 2
+was 8 9 3 6 7
+we 2 4
+well 9 4 3
+when 1 4 8
+which 8
+who 6 2
+will 2
+wished 7
+with 3 5 4
+woman 2
+women 5
+wooden 3
+worlds 10
+worms 5
+worn 7
+wrapped 4
+year 1
+years 3
+yellow 2
+you 9 3
+younger 7
