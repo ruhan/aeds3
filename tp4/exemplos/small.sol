085f46f66b8854839aeb6439174321d9819a901b
Bruna Peres <buhk06@gmail.com>
2014-06-08 11:08:23 -0300

    Create small.sol

---
+++ tp4/exemplos/small.sol
@@ -0,0 +1,36 @@
+3.0:when,0.0 i,0.0 wake,0.0 up,0.0 the,0.0 other,0.0 side,0.0 of,0.0 the,0.0 bed,0.0 is,0.0 cold,0.0 my,0.0 fingers,3.0 stretch,0.0 out,0.0 seeking,0.0
+2.0:prims,0.0 warmth,0.0 but,0.0 finding,0.0 only,0.0 the,0.0 rough,0.0 canvas,2.0 cover,0.0 of,0.0 the,0.0 mattress,0.0 she,0.0 must,0.0 have,0.0
+2.0:had,0.0 bad,0.0 dreams,0.0 and,0.0 climbed,2.0 in,0.0 with,0.0 our,0.0 mother,0.0 of,0.0 course,0.0 she,0.0 did,0.0 this,0.0 is,0.0 the,0.0 day,0.0 of,0.0 the,0.0
+2.0:reaping,2.0 i,0.0 prop,0.0 myself,0.0 up,0.0 on,0.0 one,0.0 elbow,0.0 theres,0.0
+0.0:enough,0.0 light,0.0 in,0.0 the,0.0 bedroom,0.0 to,0.0 see,0.0 them,0.0 my,0.0 little,0.0 sister,0.0 prim,0.0 curled,0.0 up,0.0 on,0.0 her,0.0 side,0.0
+0.0:cocooned,0.0 in,0.0 my,0.0 mothers,0.0 body,0.0 their,0.0 cheeks,0.0 pressed,0.0 together,0.0 in,0.0 sleep,0.0 my,0.0 mother,0.0 looks,0.0
+1.5:younger,0.0 still,0.0 worn,0.0 but,0.0 not,0.0 so,0.0 beaten,0.0 down,0.0 prims,0.0 face,0.0 is,0.0 as,0.0 fresh,0.0 as,0.0 a,0.0 raindrop,1.5 as,0.0 lovely,0.0
+0.0:as,0.0 the,0.0 primrose,0.0 for,0.0 which,0.0 she,0.0 was,0.0 named,0.0
+0.0:my,0.0 mother,0.0 was,0.0 very,0.0 beautiful,0.0 once,0.0 too,0.0 or,0.0 so,0.0 they,0.0 tell,0.0 me,0.0 sitting,0.0 at,0.0 prims,0.0 knees,0.0
+2.0:guarding,0.0 her,0.0 is,0.0 the,0.0 worlds,0.0 ugliest,2.0 cat,0.0 mashed,0.0 in,0.0 nose,0.0 half,0.0 of,0.0 one,0.0 ear,0.0 missing,0.0
+0.0:eyes,0.0 the,0.0 color,0.0 of,0.0 rotting,0.0 squash,0.0 prim,0.0 named,0.0 him,0.0 buttercup,0.0 insisting,0.0 that,0.0 his,0.0
+0.0:muddy,0.0 yellow,0.0 coat,0.0 matched,0.0 the,0.0 bright,0.0 flower,0.0 i,0.0 he,0.0 hates,0.0 me,0.0 or,0.0 at,0.0
+0.0:least,0.0 distrusts,0.0 me,0.0 even,0.0 though,0.0 it,0.0 was,0.0 years,0.0 ago,0.0 i,0.0 think,0.0 he,0.0 still,0.0 remembers,0.0
+0.0:how,0.0 i,0.0 tried,0.0 to,0.0 drown,0.0 him,0.0 in,0.0 a,0.0 bucket,0.0 when,0.0 prim,0.0 brought,0.0 him,0.0 home,0.0 scrawny,0.0
+0.0:kitten,0.0 belly,0.0 swollen,0.0 with,0.0 worms,0.0 crawling,0.0 with,0.0 fleas,0.0 the,0.0 last,0.0 thing,0.0 i,0.0 needed,0.0
+0.0:was,0.0 another,0.0 mouth,0.0 to,0.0 feed,0.0 but,0.0 prim,0.0 begged,0.0 so,0.0 hard,0.0 cried,0.0 even,0.0 i,0.0 had,0.0 to,0.0 let,0.0 him,0.0
+0.0:stay,0.0 it,0.0 turned,0.0 out,0.0 okay,0.0 my,0.0 mother,0.0 got,0.0 rid,0.0 of,0.0 the,0.0 vermin,0.0 and,0.0 hes,0.0 a,0.0 born,0.0
+0.0:mouser,0.0 even,0.0 catches,0.0 the,0.0 occasional,0.0 rat,0.0 sometimes,0.0 when,0.0 i,0.0 clean,0.0 a,0.0 kill,0.0
+0.0:i,0.0 feed,0.0 buttercup,0.0 the,0.0 entrails,0.0 he,0.0 has,0.0 stopped,0.0 hissing,0.0 at,0.0 me,0.0 entrails,0.0 no,0.0 hissing,0.0 this,0.0 is,0.0
+0.0:the,0.0 closest,0.0 we,0.0 will,0.0 ever,0.0 come,0.0 to,0.0 love,0.0 i,0.0 swing,0.0 my,0.0 legs,0.0 off,0.0 the,0.0 bed,0.0 and,0.0
+0.0:slide,0.0 into,0.0 my,0.0 hunting,0.0 boots,0.0 supple,0.0 leather,0.0 that,0.0 has,0.0 molded,0.0 to,0.0 my,0.0 feet,0.0
+0.0:i,0.0 pull,0.0 on,0.0 trousers,0.0 a,0.0 shirt,0.0 tuck,0.0 my,0.0 long,0.0 dark,0.0 braid,0.0 up,0.0 into,0.0 a,0.0 cap,0.0 and,0.0 grab,0.0
+1.5:my,0.0 forage,1.5 bag,0.0 on,0.0 the,0.0 table,0.0 under,0.0 a,0.0 wooden,0.0 bowl,0.0 to,0.0 protect,0.0 it,0.0 from,0.0 hungry,0.0 rats,0.0 and,0.0 cats,0.0 alike,0.0
+2.5:sits,0.0 a,0.0 perfect,0.0 little,0.0 goat,0.0 cheese,0.0 wrapped,2.5 in,0.0 basil,0.0 leaves,0.0 prims,0.0 gift,0.0 to,0.0
+0.0:me,0.0 on,0.0 reaping,0.0 day,0.0 i,0.0 put,0.0 the,0.0 cheese,0.0 carefully,0.0 in,0.0 my,0.0 pocket,0.0 as,0.0 i,0.0 slip,0.0 outside,0.0
+0.0:our,0.0 part,0.0 of,0.0 district,0.0 nicknamed,0.0 the,0.0 seam,0.0 is,0.0 usually,0.0 crawling,0.0 with,0.0 coal,0.0 miners,0.0
+0.0:heading,0.0 out,0.0 to,0.0 the,0.0 morning,0.0 shift,0.0 at,0.0 this,0.0 hour,0.0 men,0.0 and,0.0 women,0.0 with,0.0 hunched,0.0 shoulders,0.0
+0.0:swollen,0.0 knuckles,0.0 many,0.0 who,0.0 have,0.0 long,0.0 since,0.0 stopped,0.0 trying,0.0 to,0.0 scrub,0.0 the,0.0
+2.0:coal,0.0 dust,0.0 out,0.0 of,0.0 their,0.0 broken,2.0 nails,0.0 the,0.0 lines,0.0 of,0.0 their,0.0 sunken,0.0 faces,0.0 but,0.0 today,0.0
+0.0:the,0.0 black,0.0 cinder,0.0 streets,0.0 are,0.0 empty,0.0 shutters,0.0 on,0.0 the,0.0 squat,0.0 gray,0.0 houses,0.0
+0.0:are,0.0 closed,0.0 the,0.0 reaping,0.0 isnt,0.0 until,0.0 two,0.0 may,0.0 as,0.0 well,0.0 sleep,0.0 in,0.0 if,0.0 you,0.0 can,0.0 our,0.0 house,0.0 is,0.0 almost,0.0
+0.0:a,0.0 squirrel,0.0 think,0.0 the,0.0 old,0.0 man,0.0 was,0.0 feeling,0.0 sentimental,0.0 this,0.0 morning,0.0 says,0.0 gale,0.0 even,0.0 wished,0.0
+0.0:me,0.0 luck,0.0 well,0.0 we,0.0 all,0.0 feel,0.0 a,0.0 little,0.0 closer,0.0 today,0.0 dont,0.0 we,0.0 i,0.0 say,0.0 not,0.0 even,0.0 bothering,0.0 to,0.0 roll,0.0 my,0.0 eyes,0.0 prim,0.0 left,0.0 us,0.0 a,0.0 cheese,0.0 i,0.0 pull,0.0
+0.0:it,0.0 out,0.0 his,0.0 expression,0.0 brightens,0.0 at,0.0 the,0.0 treat,0.0 thank,0.0 you,0.0 prim,0.0 well,0.0 have,0.0 a,0.0 real,0.0 feast,0.0 suddenly,0.0
+0.0:he,0.0 falls,0.0 into,0.0 a,0.0 capitol,0.0 accent,0.0 as,0.0 he,0.0 mimics,0.0 effie,0.0 trinket,0.0 the,0.0 maniacally,0.0 upbeat,0.0 woman,0.0 who,0.0
+0.0:arrives,0.0 once,0.0 a,0.0 year,0.0 to,0.0 read,0.0 out,0.0 the,0.0 names,0.0 at,0.0 the,0.0 leaping,0.0 i,0.0 almost,0.0 forgot,0.0 happy,0.0 hunger,0.0 games,0.0 he,0.0 plucks,0.0 a,0.0 few,0.0 blackberries,0.0 from,0.0
